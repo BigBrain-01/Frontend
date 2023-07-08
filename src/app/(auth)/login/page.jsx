a72573dc8d3043a232cf5b0b797c4f45a9d47658
ayushsingh0607 <ayushraj0607@gmail.com>
2023-07-08 10:04:49 +0530

    ayush commits recent changes.

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,41 +1,45 @@
-'use-client'
-import React from 'react'
-import Image from 'next/image'
+'use client'
+import React, { useState } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import { signIn } from 'next-auth/react'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
 
 const login = () => {
+  const [show, setShow] = useState(true);
   return (
     <div className='flex '>
-      <div className='w-[40%] h-[700px]'>
-        <Image src='/image.png' width={658} height={1024} />
-      </div>
-      <div className='w-[60%] items-center justify-center flex flex-col gap-10'>
-        <div className='flex w-[509px] h-[72px] font-poppins gap-3'>
-          <h1 className='text-[48px] font-semibold text-[#043133] leading-[72px]'>Welcome To</h1>
-          <h1 className='text-[48px] font-semibold text-[#317B74] leading-[72px]'>BigBrain</h1>
+      <div className='w-[450px] h-[100vh] bg-[url(/image.png)] bg-cover bg-center bg-no-repeat '></div>
+      <div className='w-[70%] items-center justify-center flex flex-col gap-3'>
+        <div className='flex w-full h-[72px] font-poppins gap-3 justify-center'>
+          <h1 className='text-4xl font-semibold text-[#043133] leading-normal'>Welcome To</h1>
+          <h1 className='text-4xl font-semibold text-[#317B74] leading-normal'>StudyTitans</h1>
         </div>
-        <div className='flex flex-col items-center justify-center gap-8'>
-          <div className='flex flex-col'>
-            <span className='text-[20px] font-[500] text-[#4D5959] font-poppins leading-[30px]'>Username</span>
-            <input type="text" placeholder="Enter your username here" className='w-[600px] h-[65px] bg-[#EFF0F2] p-5 text-[20px] placeholder:text-[
-#838383] placeholder:text-[20px] placeholder:leading-[30px]' style={{ outline: 'none', border: 'none' }} />
+        <div className='flex flex-col items-center justify-center gap-8 w-1/2'>
+          <div className='flex flex-col w-full'>
+            <span className='font-[500] text-[#4D5959] font-poppins leading-normal'>Email</span>
+            <input type="text" placeholder="Enter your email here" className=' w-full bg-[#EFF0F2] p-3  placeholder:text-[#838383] placeholder:leading-normal' style={{ outline: 'none', border: 'none' }} />
           </div>
-          <div className='flex flex-col'>
-            <span className='text-[20px] font-[500] text-[#4D5959] font-poppins leading-[30px]'>Password</span>
-            <input type="password" placeholder="Enter your password" className='w-[600px] h-[65px] bg-[#EFF0F2] p-5 text-[20px] placeholder:text-[
-#838383] placeholder:text-[20px] placeholder:leading-[30px]' style={{ outline: 'none', border: 'none' }} />
-          </div>
-          <div className='flex flex-col gap-3'>
-            <button className='w-[280px] h-[60px] bg-[#007074] text-[#FFFFFF] text-[26px] font-[500] font-poppins leading-[36px]' >Login</button>
-            <div className='flex items-center justify-center'>
-              <span className='text-[#4D5959] text-[18px] font-[500] leading-[27px]'>Don't have an account?</span>
-              <Link href="/register">
-                <span className='text-[#007074] text-[18px] font-[500] leading-[27px] cursor-pointer'>Sign up</span>
-              </Link>
+          <div className='flex flex-col w-full'>
+            <span className='font-[500] text-[#4D5959] font-poppins leading-[30px]'>Password</span>
+            <div className='w-full bg-[#EFF0F2] p-3 flex justify-between'>
+              <input type={show ? "password" : "text"} placeholder="Enter your password" className=' placeholder:text-[#838383] placeholder:leading-normal bg-[#EFF0F2]' style={{ outline: 'none', border: 'none',width:'90%' }} />
+              <div className='cursor-pointer' onClick={()=>{setShow(!show)}}>{!show?<Visibility sx={{color:'#4D5959'}}/>:<VisibilityOff sx={{color:'#4D5959'}}/>}</div>
             </div>
           </div>
-          <div>
-            <span className='text-[26px] text-[#043133] font-poppins leading-[39px] font-[500]'>OR</span>
+          <div className='flex flex-col gap-3'>
+            <button className='w-max px-24 py-2 bg-[#007074] text-[#FFFFFF] text-[20px] font-medium font-poppins leading-normal' >Login</button>
+            <div className='text-[#4D5959] flex justify-center text-sm gap-1'>
+              Don't have an account?
+              <Link className='text-[#007074] font-medium' href={'/register'}>Sign up</Link>
+            </div>
+          </div>
+          <div className='flex flex-col items-center gap-3'>
+            <span className='text-[16px] text-[#043133] font-poppins leading-normal font-medium'>OR</span>
+            <button onClick={() => signIn("google")} className='flex gap-2 px-4 py-2 items-center justify-center border-[1.4px] border-[#D2D2D2] text-[#043133] text-sm leading-normal font-medium'>
+              <Image src={'/google.png'} alt='' width={30} height={30} />
+              Login with Google
+            </button>
           </div>
         </div>
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -21,7 +21,22 @@ export default function Home() {
           <Image src={'/u.png'} alt='' width={'80'} quality={100} className='absolute top-20 -right-10' height={'120'} />
           <Image src={'/greenArrow.png'} alt='' width={'80'} quality={100} className='absolute bottom-20 -right-20' height={'120'} />
           <Image src={'/graph.png'} alt='' width={'80'} quality={100} className='absolute bottom-28 left-28' height={'120'} />
-
+        </div>
+      </div>
+      {/* about section */}
+      <div className='px-32 flex flex-col justify-center py-20' id='about'>
+        <div className='flex justify-between'>
+          <div className='flex flex-col'>
+            <span className='text-[20px] text-[#0015C8] font-bold font-inter leading-normal'>About Us</span>
+            <div className='text-4xl font-inter text-[#18191F] font-[800] leading-normal'>An <span className='relative' >Easier
+              <Image className='absolute -bottom-4 left-0' src={'/underline.png'} alt='' width={200} height={44} />
+            </span> Way to be<br /> Sub and Find Sub
+            </div>
+          </div>
+          <div className='flex relative '>
+            <p className='w-[546px] text-[20px] text-[#757575] font-inter font-semibold leading-normal'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also</p>
+            <Image src={'/arrow.png'} alt='' width={'150'} quality={100} height={'120'} className='absolute right-[34rem] -bottom-[3.5rem]'></Image>
+          </div>
         </div>
       </div>
     </div>
