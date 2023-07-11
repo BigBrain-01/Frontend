dfdd3bdf1cb1b435c798e11cfdb267ef53959d25
Darpanarayan Bahadur <92776555+SkullCreek@users.noreply.github.com>
2023-07-12 01:35:38 +0530

    sign up

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -10,22 +10,24 @@ const login = () => {
   return (
     <div className='flex '>
       <div className='w-[50%] h-[100vh] bg-[url(/image.jpg)] bg-cover bg-center bg-no-repeat'></div>
-      <div className='w-[50%] py-10 flex flex-col items-center bg-[url(/login-bg.png)] bg-cover bg-center bg-no-repeat'>
+      <div className='w-[50%] h-[100vh] overflow-y-auto py-10 flex flex-col items-center bg-[url(/login-bg.png)] bg-cover bg-center bg-no-repeat'>
         <div className='flex gap-3 justify-center pt-5 pb-8'>
           <h1 className='text-4xl font-semibold text-[#043133] leading-normal'>Welcome To</h1>
           <h1 className='text-4xl font-semibold text-[#317B74] leading-normal'>StudyTitans</h1>
         </div>
         <div className='w-[64%]'>
-          <div className='flex flex-col w-full pb-6'>
+          <div className='flex flex-col w-full pb-3'>
             <span className='font-[500] text-[#4D5959] leading-[30px] pb-0.2'>Username</span>
             <input type="text" placeholder="Enter your email here" className=' w-full bg-[#EFF0F2] px-6 py-3  placeholder:text-[#838383] placeholder:leading-normal' style={{ outline: 'none', border: 'none' }} />
+            <p className='h-[20px] text-red-500 text-[15px]'>&#9888; name not valid</p>
           </div>
-          <div className='flex flex-col w-full pb-10'>
+          <div className='flex flex-col w-full pb-7'>
             <span className='font-[500] text-[#4D5959] leading-[30px] pb-0.2'>Password</span>
             <div className='relative'>
               <input type={show ? "password" : "text"} placeholder="Enter your password" className='px-6 py-3 w-[100%] placeholder:text-[#838383] placeholder:leading-normal bg-[#EFF0F2]' style={{ outline: 'none', border: 'none'}} />
               <div className='cursor-pointer absolute top-2.5 right-4' onClick={()=>{setShow(!show)}}>{!show?<Visibility sx={{color:'#4D5959'}}/>:<VisibilityOff sx={{color:'#4D5959'}}/>}</div>
             </div>
+            <p className='h-[20px] text-red-500 text-[15px]'>&#9888; name not valid</p>
             <div className='text-[#317B74] w-full font-[500] text-right pt-1'>forget Password?</div>
           </div>
           <div className='flex flex-col items-center pb-12'>
