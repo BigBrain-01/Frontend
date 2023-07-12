0453df6fe433e88cc2f37c2266ac86175d4d8faa
ayushsingh0607 <80676654+ayushsingh0607@users.noreply.github.com>
2023-07-12 22:36:18 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,7 @@ export default function RootLayout({children}) {
     <html lang="en" className='scroll-smooth'>
       <body suppressHydrationWarning={true}>
         <AuthProvider>
+    <SessionProvider>
           {children}
           <Footer/>
         </SessionProvider>
