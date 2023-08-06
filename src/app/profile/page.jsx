d422858c7babfc4d3e31fd7d71c7f49f441dee01
ayushsingh0607 <ayushraj0607@gmail.com>
2023-08-06 21:56:36 +0530

    ayush commits profile

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -4,16 +4,32 @@ import Sidebar from '@/components/Sidebar/Sidebar'
 import SearchIcon from "@mui/icons-material/Search";
 import Image from "next/image";
 import EditIcon from '@mui/icons-material/Edit';
-import { Button } from '@mui/material';
+import { Button, Select } from '@mui/material';
 import moment from "moment";
+import MenuItem from '@mui/material/MenuItem';
+import { KeyboardArrowDown } from '@mui/icons-material';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import dayjs from 'dayjs';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+
 
 const page = () => {
     const [general, setGeneral] = useState(true)
     const [security, setSecurity] = useState(false);
+    const [item, setItem] = useState('')
+    const [opencalendar, setOpenCalendar] = useState(false)
+    const [value, setValue] = useState(dayjs('2022-04-17'))
+
+    const handleChange = (event) => {
+        setItem(event.target.value);
+    };
     return (
-        <div className='flex'>
+        <div className='flex '>
             <Sidebar />
-            <div className='bg-[#EAECFE] flex flex-col w-full px-5 gap-7'>
+            <div className='bg-[#EAECFE] flex flex-col w-full px-5 gap-7 h-full'>
                 <div className="flex  py-5 gap-5 bg-[#EAECFE]  items-center justify-between w-full h-14">
                     <div className="flex gap-5 items-center">
                         <div className="flex flex-col text-xs">
@@ -51,8 +67,8 @@ const page = () => {
                 </div>
                 <div>
                     {general &&
-                        <div className='flex w-full gap-5 max-h'>
-                            <div className='bg-[#FFF] p-3 rounded-md flex flex-col gap-5 h-[960px]'>
+                        <div className='flex w-full gap-5'>
+                            <div className='bg-[#FFF] p-3 rounded-md flex flex-col gap-5'>
                                 <div className='flex justify-between items-center'>
                                     <span className='text-[#00261C] text-[20px] font-poppins font-semibold leading-normal'>Profile</span>
                                     <div className='flex gap-[2px] items-center'>
@@ -72,6 +88,36 @@ const page = () => {
                                         <span className='text-[#A0A0A0] font-semibold font-poppins leading-normal'>Last Name</span>
                                         <div className='border-[#EAEAEA] w-[360px] border-[2px] rounded-md p-2'>
                                             <input type="text" placeholder='John Doe' style={{ border: 'none', outline: "none" }} className='w-full' />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='flex gap-3'>
+                                    <div>
+                                        <span className='text-[#A0A0A0] font-semibold font-poppins leading-normal'>Gender</span>
+                                        <div className='w-[360px] rounded-md'>
+                                            <Select onChange={handleChange} value={item} size='small' sx={{ width: '100%', '.MuiSvgIcon-root ': { fill: "black !important" }, color: 'black', border: 'none', outline: 'none' }} IconComponent={KeyboardArrowDown} >
+                                                <MenuItem value={'HELLO'}>Male</MenuItem>
+                                                <MenuItem value={'hEY'}>Female</MenuItem>
+                                                <MenuItem value={'HII'}>I do not prefer to tell</MenuItem>
+                                            </Select>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <span className='text-[#A0A0A0] font-semibold font-poppins leading-normal'>Date Of Birth</span>
+                                        <div className='border-[#EAEAEA] w-[360px] border-[2px] rounded-md p-2 flex justify-between relative' onClick={() => setOpenCalendar(!opencalendar)}>
+                                            <span>06/08/2023</span>
+                                            <CalendarMonthIcon color='gray' sx={{ width: '20px', height: '20px', cursor: 'pointer' }} />
+                                            <div className='z-20 absolute bg-[#FFF] top-14'>
+                                                {opencalendar &&
+                                                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                                        <DemoContainer components={['DateCalendar', 'DateCalendar']}>
+                                                            <DemoItem>
+                                                                <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
+                                                            </DemoItem>
+                                                        </DemoContainer>
+                                                    </LocalizationProvider>
+                                                }
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -153,12 +199,39 @@ const page = () => {
                                     <Button variant='contained' type='none' sx={{ textTransform: 'capitalize', padding: "9px 35px", fontSize: '16px' }}>Update</Button>
                                 </div>
                             </div>
-                            <div className='w-[298px] h-[435px]  bg-[#FFF]'>
-                                <div>
-                                    <Image src='/profile.png' width={'100'} height={'100'} className='rounded-[50%]' />
-
+                            <div className='  bg-[#FFF] h-[435px]'>
+                                <div className='flex flex-col justify-center px-3 py-7 gap-7'>
+                                    <div className='relative flex justify-center'>
+                                        <Image src='/profile.png' width={'80'} height={'80'} className='rounded-[50%]' />
+                                        <span className='w-[30px] h-[30px] bg-[#FAFAFA] rounded-full absolute top-14 right-[7rem] flex justify-center items-center cursor-pointer'><EditIcon sx={{ width: '16px', height: '16px' }} /></span>
+                                    </div>
+                                    <div className='flex flex-col gap-5'>
+                                        <div className='flex gap-2'>
+                                            <span className='text-[16px] text-[#000] font-semibold font-poppins leading-[24px]'>Name :</span>
+                                            <span className='text-[16px] text-[#A0A0A0] font-semibold font-poppins leading-[24px] text-right'>darpan bahadur</span>
+                                        </div>
+                                        <div className='flex gap-2'>
+                                            <span className='text-[16px] text-[#000] font-semibold font-poppins leading-[24px] w-[135px]'>Registration No.:</span>
+                                            <span className='text-[16px] text-[#A0A0A0] font-semibold font-poppins leading-[24px]'>342</span>
+                                        </div>
+                                        <div className='flex gap-2'>
+                                            <span className='text-[16px] text-[#000] font-semibold font-poppins leading-[24px] '>Email:</span>
+                                            <span className='text-[16px] text-[#A0A0A0] font-semibold font-poppins leading-[24px]'>darpanbahadur123@gmail.com</span>
+                                        </div>
+                                        <div className='flex gap-2'>
+                                            <span className='text-[16px] text-[#000] font-semibold font-poppins leading-[24px] '>Phone:</span>
+                                            <span className='text-[16px] text-[#A0A0A0] font-semibold font-poppins leading-[24px]'>7894561230</span>
+                                        </div>
+                                        <div className='flex gap-2'>
+                                            <span className='text-[16px] text-[#000] font-semibold font-poppins leading-[24px] '>Gender:</span>
+                                            <span className='text-[16px] text-[#A0A0A0] font-semibold font-poppins leading-[24px]'>Male</span>
+                                        </div>
+                                        <div className='flex gap-2'>
+                                            <span className='text-[16px] text-[#000] font-semibold font-poppins leading-[24px] '>DOB:</span>
+                                            <span className='text-[16px] text-[#A0A0A0] font-semibold font-poppins leading-[24px]'>14/12/2023</span>
+                                        </div>
+                                    </div>
                                 </div>
-
                             </div>
                         </div>
                     }
